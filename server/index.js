3b1dff6041e77078be0c273a7118b0427368916e
ewalker09 <93848765+ewalker09@users.noreply.github.com>
2022-03-22 18:15:14 -0400

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://Peanuts123:Lola123@cluster0.dawcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server running on port: ${PORT}`)})
+
+
+// https://www.mongodb.com/cloud/atlas
